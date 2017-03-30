3618719da7c157d897acb5a316128646bce2f8bb
yxy yxy <yxy yxy>
2017-03-30 22:27:19 +0800

    chaos

--- test.cpp
+++ test.cpp
@@ -2,6 +2,8 @@
 #include<cstdio>
 #include<fstream>
 #include<cstring>
+#define debug
+#undef debug
 using namespace std;
 const int N=100000;
 char code[N];
@@ -9,17 +11,42 @@ int data[N];
 int runPtr=N>>1;
 int main(int argc,char** argv){
 	ifstream ifile;
-	//ifile.open(argv[1]);
-	ifile.open("test.bt");
-	if(!ifile.is_open())return 1;
+	ifile.open(argv[1]);
+	//ifile.open("test.bt");
+	if(!ifile.is_open()){
+		puts("Can not open file!");
+		return 1;
+	}
+#ifdef debug
+	puts("File opened!");
+#endif
 	int p=0;
 	char c;
-	while(ifile>>c)code[p++]=c;
+	while(ifile>>c)
+		switch(c){
+			case '.':
+			case ',':
+			case '<':
+			case '>':
+			case '[':
+			case ']':
+			case '+':
+			case '-':
+				code[p++]=c;
+				break;
+			default:
+				break;
+		}
 	ifile.close();
+#ifdef debug
 	cout<<code<<endl;
+#endif
 	p=0;
 	int len=strlen(code);
 	while(p<len){
+#ifdef debug
+		printf("code prt=%d data ptr=%d\n",p,runPtr);
+#endif
 		switch(code[p]){
 			case '>':
 				runPtr++;
